337b5dbc4cbc5fc654c0b2bfb8b4af8f56842e69
albertly <albertly@yandex.ru>
2020-09-25 20:57:16 +0300

    InsDetails

--- src/InsDetails.tsx
+++ src/InsDetails.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import Button from 'react-bootstrap/Button'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './InsDetails.css';
+
 function InsDetails(): React.ReactElement {
     let history = useHistory();
 
@@ -10,10 +13,33 @@ function InsDetails(): React.ReactElement {
     }
 
     return (
-        <div>
-            InsDetails
+        <>
+            <section>
+                <div className="row row-one" >
+                    <div className="col-3 col-first">
+                        1 of 2
+                    </div>
+                    <div className="col">
+                        2 of 2
+                    </div>
+                    <div className="col">
+                        3 of 4
+                    </div>
+                    <div className="col col-last">
+                        4 of 4
+                    </div>
+                </div>
+                <div className="row row-two">
+                    <div className="col-3 col-first">
+                        1 of 2
+                    </div>
+                    <div className="col col-last">
+                        2 of 2
+                    </div>
+                </div>
+            </section>
             <Button variant="primary" onClick={handleClick}>המשך</Button>
-        </div>
+        </>
     )
 }
 export default InsDetails;
