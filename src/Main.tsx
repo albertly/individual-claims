4a673803e3000e24d124cee6e6f643d32a74f063
albertly <albertly@yandex.ru>
2020-09-26 19:07:43 +0300

    Building Main

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import Button from 'react-bootstrap/Button'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './Main.module.css';
+
 function Main(): React.ReactElement {
     let history = useHistory();
 
@@ -11,6 +14,28 @@ function Main(): React.ReactElement {
 
     return (
         <div>
+            <section>
+
+                <div className={`row justify-content-center ${styles.row_one}`} >
+                    <div className="col-3 col-first">
+                        רגע לפני שמתחילים...
+                    </div>
+                    <div className="col col-last col-text-help">
+                        <p>
+                            כדי שנוכל להתקדם בזריזות, אנחנו מבקשים שסריקה או צילום של המסמכים תהיה ההישג ידך:
+                        </p>
+                    </div>
+                </div>
+                <div className={`row ${styles.row_two}`}>
+                    <div className="col-3 col-first">
+                        1 of 2
+                    </div>
+                    <div className="col col-last">
+                        2 of 2
+                    </div>
+                </div>
+            </section>
+
             <Button variant="primary" onClick={handleClick}>המשך</Button>
         </div>
     )
