bab7482c12458c870b30c56bb93cc6d9ed21e03d
albertly <albertly@yandex.ru>
2020-09-28 20:06:10 +0300

    InsDetails responsive layout

--- src/InsDetails.tsx
+++ src/InsDetails.tsx
@@ -1,45 +1,42 @@
 import React, { useState } from 'react';
-import { useHistory } from "react-router-dom";
-import Button from 'react-bootstrap/Button'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './InsDetails.css';
+import { Form } from 'react-bootstrap';
 
 function InsDetails(): React.ReactElement {
-    let history = useHistory();
-
-    function handleClick() {
-        history.push("/second");
-    }
-
-    return (
-        <>
-            <section>
-                <div className="row row-one" >
-                    <div className="col-3 col-first">
-                        1 of 2
-                    </div>
-                    <div className="col">
-                        2 of 2
-                    </div>
-                    <div className="col">
-                        3 of 4
-                    </div>
-                    <div className="col col-last">
-                        4 of 4
-                    </div>
-                </div>
-                <div className="row row-two">
-                    <div className="col-3 col-first">
-                        1 of 2
-                    </div>
-                    <div className="col col-last">
-                        2 of 2
-                    </div>
-                </div>
-            </section>
-            {/* <Button variant="primary" onClick={handleClick}>המשך</Button> */}
-        </>
-    )
+  return (
+    <>
+      <section>
+        <div className="row row-one">
+          <div className="col-md-3 col-xs-12 col-first">
+            פרטי המבוטח לגביו מוגשת התביעה
+          </div>
+          <div className="col-md-3 col-xs-12 col-bl">
+            <Form>
+              <div className="mb-1">
+                <Form.Check
+                  className="form-control form-control-lg"
+                  size={10}
+                  inline
+                  label="123456789"
+                  type="radio"
+                  id={`inline--1`}
+                />
+              </div>
+            </Form>
+          </div>
+          <div className="col-md-3 col-xs-12 col-bl">3 of 4</div>
+          <div className="col-md-3 col-xs-12 col-last">4 of 4</div>
+        </div>
+        <div className="row row-two">
+          <div className="col-md-3 col-xs-12 col-first">
+            פרטי יצירת קשר עם המבוטח
+          </div>
+          <div className="col-md-9 col-xs-12 col-last">2 of 2</div>
+        </div>
+      </section>
+    </>
+  );
 }
 export default InsDetails;
