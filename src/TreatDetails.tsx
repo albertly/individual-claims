697085cdd8e10d6dfc39474e539f238b33be3f37
albertly <albertly@yandex.ru>
2020-10-01 14:04:47 +0300

    TreadDetails css

--- src/TreatDetails.tsx
+++ src/TreatDetails.tsx
@@ -1,41 +1,35 @@
 import React, { useState } from 'react';
-import { useHistory } from "react-router-dom";
-import Button from 'react-bootstrap/Button'
+import { useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
-import styles from './TreatDetails.module.css';
+import Button from 'react-bootstrap/Button';
 
 function TreadDetails(): React.ReactElement {
-    let history = useHistory();
+  let history = useHistory();
 
-    function handleClick() {
-        history.push("/second");
-    }
+  function handleClick() {
+    history.push('/second');
+  }
 
-    return (
-        <div>
-            <section>
-
-                <div className={`row justify-content-center ${styles.row_one}`} >
-                    <div className="col-3 col-first">
-                        רגע לפני שמתחילים...
-    </div>
-                    <div className="col col-last col-text-help">
-                        <p>
-                            כדי שנוכל להתקדם בזריזות, אנחנו מבקשים שסריקה או צילום של המסמכים תהיה ההישג ידך:
-        </p>
-                    </div>
-                </div>
-                <div className={`row ${styles.row_two}`}>
-                    <div className="col-3 col-first">
-                        1 of 2
-    </div>
-                    <div className="col col-last">
-                        2 of 2
-    </div>
-                </div>
-            </section>
-            {/* <Button variant="primary" onClick={handleClick}>המשך</Button> */}
+  return (
+    <main aria-label="פרטי טיפול">
+      <section className="row" aria-labelledby="treatDetails">
+        <h3 id="treatDetails" className="col-3 col-first">
+          פרטי הטיפול/ים
+        </h3>
+        <div className="col col-last col-text-help">
+          <p>
+            כדי שנוכל להתקדם בזריזות, אנחנו מבקשים שסריקה או צילום של המסמכים
+            תהיה ההישג ידך:
+          </p>
         </div>
-    )
+      </section>
+      <section className="row">
+        <div className="col-3 col-first">1 of 2</div>
+        <div className="col col-last">2 of 2</div>
+      </section>
+      {/* <Button variant="primary" onClick={handleClick}>המשך</Button> */}
+    </main>
+  );
 }
 export default TreadDetails;
