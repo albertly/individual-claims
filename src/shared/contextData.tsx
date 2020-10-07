8bf925d1b2f3a210e8797594ec0d60b478ecd5dc
albertly <albertly@yandex.ru>
2020-10-07 11:14:57 +0300

    Context refactoring

--- src/shared/contextData.tsx
+++ src/shared/contextData.tsx
@@ -1,89 +1,19 @@
 /* eslint-disable eqeqeq */
 import React, { useReducer } from 'react';
 
-type ActionMap<M extends { [index: string]: any }> = {
-  [Key in keyof M]: M[Key] extends undefined
-    ? {
-        type: Key;
-      }
-    : {
-        type: Key;
-        payload: M[Key];
-      };
-};
-
-export enum Types {
-  SetIns = 'SET_DATA_INS',
-  SetTreat = 'SET_DATA_TREAT',
-  AddTreatDetail = 'ADD_TREAT_DETAIL',
-}
-
-// Insured
-export type InsType = {
-  insured: string;
-  mobile: string;
-  email: string;
-};
-
-type InsPayload = {
-  [Types.SetIns]: InsType;
-};
-
-export type InsActions = ActionMap<InsPayload>[keyof ActionMap<InsPayload>];
-
-const insReducer = (state: InsType, action: InsActions | TreatActions) => {
-  switch (action.type) {
-    case Types.SetIns:
-      return { ...action.payload };
-    default:
-      return state;
-  }
-};
-
-// Treatment
-export type TreatDetail = {
-  treatId: number;
-  treatDate: Date;
-  cost: number;
-  notes: string;
-  tooth?: number;
-  CL_V?: boolean;
-  L_P?: boolean;
-  B?: boolean;
-  D?: boolean;
-  M?: boolean;
-  O?: boolean;
-};
-
-export type TreatType = {
-  invoice: number;
-  doctorId?: number;
-  treatments: TreatDetail[];
-};
-
-type TreatPayload = {
-  [Types.SetTreat]: TreatType;
-  [Types.AddTreatDetail]: TreatDetail;
-};
-
-export type TreatActions = ActionMap<TreatPayload>[keyof ActionMap<
-  TreatPayload
->];
-
-const treatReducer = (state: TreatType, action: InsActions | TreatActions) => {
-  
-  switch (action.type) {
-    case Types.SetTreat:
-      return { ...action.payload };
-    case Types.AddTreatDetail:
-      return {...state, treatments: [...state.treatments, {...action.payload}]};
-    default:
-      return state;
-  }
-};
+import {
+  insReducer,
+  treatReducer,
+  InsActions,
+  TreatActions,
+  InsType,
+  TreatType,
+  TreatDetail,
+  Types,
+} from './reducersData';
 
 // Context
-export type InitialStateType = {
+type InitialStateType = {
   insured: InsType;
   treatment: TreatType;
 };
@@ -125,4 +55,5 @@ const DataProvider: React.FC = ({ children }) => {
   );
 };
 
-export { DataContext, DataProvider };
+export { DataContext, DataProvider, Types };
+export type { InsType, TreatType, TreatDetail };
---
+++ src/shared/reducersData.tsx
@@ -0,0 +1,88 @@
+type ActionMap<M extends { [index: string]: any }> = {
+  [Key in keyof M]: M[Key] extends undefined
+    ? {
+        type: Key;
+      }
+    : {
+        type: Key;
+        payload: M[Key];
+      };
+};
+
+export enum Types {
+  SetIns = 'SET_DATA_INS',
+  SetTreat = 'SET_DATA_TREAT',
+  AddTreatDetail = 'ADD_TREAT_DETAIL',
+}
+
+// Insured
+export type InsType = {
+  insured: string;
+  mobile: string;
+  email: string;
+};
+
+type InsPayload = {
+  [Types.SetIns]: InsType;
+};
+
+export type InsActions = ActionMap<InsPayload>[keyof ActionMap<InsPayload>];
+
+export const insReducer = (
+  state: InsType,
+  action: InsActions | TreatActions
+) => {
+  switch (action.type) {
+    case Types.SetIns:
+      return { ...action.payload };
+    default:
+      return state;
+  }
+};
+
+// Treatment
+export type TreatDetail = {
+  treatId: number;
+  treatDate: Date;
+  cost: number;
+  notes: string;
+  tooth?: number;
+  CL_V?: boolean;
+  L_P?: boolean;
+  B?: boolean;
+  D?: boolean;
+  M?: boolean;
+  O?: boolean;
+};
+
+export type TreatType = {
+  invoice: number;
+  doctorId?: number;
+  treatments: TreatDetail[];
+};
+
+type TreatPayload = {
+  [Types.SetTreat]: TreatType;
+  [Types.AddTreatDetail]: TreatDetail;
+};
+
+export type TreatActions = ActionMap<TreatPayload>[keyof ActionMap<
+  TreatPayload
+>];
+
+export const treatReducer = (
+  state: TreatType,
+  action: InsActions | TreatActions
+) => {
+  switch (action.type) {
+    case Types.SetTreat:
+      return { ...action.payload };
+    case Types.AddTreatDetail:
+      return {
+        ...state,
+        treatments: [...state.treatments, { ...action.payload }],
+      };
+    default:
+      return state;
+  }
+};
