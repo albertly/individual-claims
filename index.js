4c34091dfbc0c6654fbd4119ec4d5501ea3bc195
albertly <albertly@yandex.ru>
2020-10-15 10:58:27 +0300

    Add security headers to response

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const express = require('express');
 const server = express();
 const helmet = require("helmet");
  
- 
-server.use(helmet());
-
 const options = {
   index: 'index.html', //Fill path here.
 };
 
 server.use(compression());
+server.use(helmet());
+server.use(function(req, res, next) {
+    res.header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    res.header('X-Content-Type-Options', 'nosniff');
+    res.header('X-Frame-Options', 'SAMEORIGIN');    
+    next();
+});
+
 server.use('/', express.static('./build', options));
 
 const port = 5000;
